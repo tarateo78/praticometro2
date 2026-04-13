d861ea77c458bff37b92fcd388a8917327e184b9
matteo_uff <tarateo78@gmail.com>
2026-04-13 13:02:03 +0200

    abbellita tabella

--- resources/views/practices/index.blade.php
+++ resources/views/practices/index.blade.php
@@ -20,31 +20,54 @@
     </div>
 
     <table>
-        @foreach($practices as $practice)
-        <tr>
+        <thead>
+            <tr>
+                <th>Codice</th>
+                <th>Titolo</th>
+                <th>Stato</th>
+                <th>Area</th>
+                <th>Cup</th>
+                <th>Importo</th>
+                <th>Finanziamento</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($practices as $practice)
+            <tr>
 
-            <td>{{ $practice->codice }}</td>
-            <td><a href="{{ route('practices.edit', $practice) }}">🔎</a></td>
-            <td>{{ $practice->titolo }}</td>
-            <td>
-                <span style="background-color:{{ $practice->is_avvio_progettazione == true ? " lightgreen" : ""
-                    }}">P</span>
-                <span style="background-color:{{ $practice->is_avvio_progettazione == true ? " lightgreen" : ""
-                    }}">G</span>
-                <span style="background-color:{{ $practice->is_avvio_progettazione == true ? " lightgreen" : ""
-                    }}">L</span>
-                <span style="background-color:{{ $practice->is_avvio_progettazione == true ? " lightgreen" : ""
-                    }}">C</span>
-            </td>
-            <td>{{ $practice->zona }}</td>
-            <td>{{ $practice->cup }}</td>
-            <td>{{ $practice->importo }}</td>
-            <td>{{ $practice->finanziamento }}</td>
-            <td>{{ $practice->rup }}</td>
-            <td>{{ $practice->gruppo }}</td>
-            <td></td>
-        </tr>
-        @endforeach
+                <td class="text-blue-800">
+                    <a href="{{ route('practices.edit', $practice) }}" class="hover:text-black">{{ $practice->codice }}</a></td>
+                <td>{{ $practice->titolo }}</td>
+                <td>
+                    @if ($practice->is_avvio_progettazione)
+                        <span class="tag bg-yellow-100" >Prog</span>
+                    @endif
+
+                    @if($practice->is_avvio_gara)
+                        <span  class="tag bg-green-100" >Gara</span>
+                    @endif
+                    
+                    @if($practice->is_lavori_in_corso)
+                        <span  class="tag bg-blue-100" >Lavori</span>
+                    @endif
+                    
+                    @if( $practice->is_cre )
+                        <span  class="tag bg-violet-100" >Cre</span>
+                    @endif
+                </td>
+                <td>{{ $practice->zona }}</td>
+                <td>{{ $practice->cup }}</td>
+                {{-- <td class="">{{ number_format((float)str_replace(str_replace($practice->importo,".",""),",",".") , 2, "," , ".")}} €</td> --}}
+                <td class="text-right pr-2">{{ number_format( (float) str_replace( ",",".", str_replace(".","", $practice->importo) ), 2) }} €</td>
+
+                <td>{{ $practice->finanziamento }}</td>
+                <td></td>
+            </tr>
+            @endforeach
+        </tbody>
+        <tfoot>
+
+        </tfoot>
     </table>
 </body>
 
