e9be5d964c6a789c89ee6cdad09259aa1134bda1
Matteo <matteo@tiemme.cloud>
2026-04-16 20:42:04 +0200

    sistemato frontend

--- resources/views/openweb/show.blade.php
+++ resources/views/openweb/show.blade.php
@@ -5,39 +5,75 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dettaglio pratica {{ $practice->codice }}</title>
-    @vite(['resources/css/app.css', 'resources/css/frontend.css','resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/css/frontend.css', 'resources/js/app.js'])
 </head>
 
 <body>
     <h1>{{ $practice->titolo }}</h1>
+    <a href="{{ route('openweb.index') }}">← Elenco Pratiche</a>
+
     <div class="grid grid-cols-1 md:grid-cols-2">
         <dl class="grid grid-cols-2">
-            <dt> codice: </dt>
-            <dd> {{ $practice->codice }} </dd>
-            <dt>titolo</dt>
+            <dt>Codice pratica:</dt>
+            <dd>{{ $practice->codice }}</dd>
+            <dt>Titolo intervento:</dt>
             <dd>{{ $practice->titolo_esteso }}</dd>
-            <dt>zona</dt>
+            <dt>Area:</dt>
             <dd>{{ $practice->zona }}</dd>
-            <dt>strade</dt>
-            <dd>{{ $practice->strade }}</dd>
-            <dt>importo</dt>
-            <dd>{{ $practice->importo }}</dd>
-            <dt>finanziamento</dt>
+            <dt>Strade interessate:</dt>
+            <dd>
+                @if($practice->strade)
+                    @foreach (explode(",", $practice->strade) as $sp)
+                        <span>{{ $sp }}</span>
+                    @endforeach
+                @endif
+            </dd>
+            <dt>Importo:</dt>
+            <dd>
+                @php
+                    $importo = (float) str_replace(",", ".", str_replace(".", "", $practice->importo));
+                @endphp
+
+                {{ number_format($importo, 2, ",", ".") }} €
+            </dd>
+            <dt>finanziamento:</dt>
             <dd>{{ $practice->finanziamento }}</dd>
-            <dt>is_avvio_progettazione</dt>
-            <dd>{{ $practice->is_avvio_progettazione }}</dd>
-            <dt>is_avvio_gara</dt>
-            <dd>{{ $practice->is_avvio_gara }}</dd>
-            <dt>is_lavori_in_corso</dt>
-            <dd>{{ $practice->is_lavori_in_corso }}</dd>
-            <dt>is_cre</dt>
-            <dd>{{ $practice->is_cre }}</dd>
-            <dt>cre_at</dt>
-            <dd>{{ $practice->cre_at }}</dd>
-            <dt>scadenza_esecuzione</dt>
-            <dd>{{ $practice->scadenza_esecuzione }}</dd>
-            <dt>coordinate</dt>
-            <dd>{{ $practice->coordinate }}</dd>
+            <dt>Stato attuale:</dt>
+            <dd>
+                @if($practice->is_cre)
+                    <span>Lavori conclusi</span>
+                @else
+                    @if($practice->is_lavori_in_corso)
+                        <span>Lavori in corso</span>
+                    @else
+                        @if($practice->is_avvio_gara)
+                            <span>Procedura di Appalto</span>
+                        @else
+                            @if ($practice->is_avvio_progettazione)
+                                <span>Progettazione</span>
+                            @endif
+                        @endif
+                    @endif
+                @endif
+            </dd>
+            <dt>Fine Lavori:</dt>
+            <dd>
+                @if(isset($practice->cre_at))
+                    @php
+                        $data = new DateTime($practice->cre_at);
+                    @endphp
+                    {{  $data->format('d/m/Y')}}
+                @else
+                    @if (isset($practice->scadenza_esecuzione))
+                        @php
+                            $data = new DateTime($practice->scadenza_esecuzione);
+                        @endphp
+                        {{ $data->format('d/m/Y') }} <span class="text-sm italic">PRESUNTA</span>
+                    @else
+                        <span class="text-xs italic">IN DEFINIZIONE</span>
+                    @endif
+                @endif
+            </dd>
         </dl>
 
     </div>
